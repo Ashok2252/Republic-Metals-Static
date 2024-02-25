436e20caeecd89ac026730a49be88ea7bd4b07ae
Ashok2252 <ashokmali1999@gmail.com>
2024-02-25 15:07:22 +0530

    Product Details Page Completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const burger = document.querySelector(".burger");
-const navLinks = document.querySelector(".nav-links");
+// const burger = document.querySelector(".burger");
+// const navLinks = document.querySelector(".nav-links");
 
-burger.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-});
+// burger.addEventListener("click", () => {
+//   navLinks.classList.toggle("show");
+// });
